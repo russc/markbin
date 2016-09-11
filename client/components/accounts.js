41cfeccbfb7df82287eca4199ef9e2cdb4878cc7
russc <russcallahan@mac.com>
2016-09-11 12:52:07 -0500

    Adds auth and meteor methods

---
+++ client/components/accounts.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Template } from 'meteor/templating';
+import { Blaze } from 'meteor/blaze';
+
+class Accounts extends Component {
+  // lifecycle methods
+  componentDidMount() {
+    // Render the Blazz accounts from the div we
+    // just rendered in the 'render' method and place the Blaze
+    // accounts form in the div
+    this.view = Blaze.render(Template.loginButtons,
+       ReactDOM.findDOMNode(this.refs.container));
+  }
+
+  componentWillUnmount() {
+    // Go find the forms we created and destroy them
+    // We need to clean up those fomrs ourselves.
+    Blaze.remove(this.view);//Garbage collection
+  }
+
+  render() {
+    return (
+      <div ref="container"></div>
+    );
+  }
+}
+
+export default Accounts;
--- client/main.js
+++ client/main.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 
 import App from './components/app';
 
-Meteor.start(() => {
+Meteor.startup(() => {
   ReactDOM.render(<App />, document.querySelector('.render-target'));
 });
