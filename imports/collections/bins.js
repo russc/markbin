2767cca38c6fa140cbd0e3f46c47cd0b02667f48
russc <russcallahan@mac.com>
2016-09-11 17:18:38 -0500

    Adds bins components and navigation.

--- imports/collections/bins.js
+++ imports/collections/bins.js
@@ -1,10 +1,21 @@
 import { Mongo } from 'meteor/mongo';
 
+// fat arrow functions bind to this.  That is why
+// we can't use fat arrow here.  The value of this inside
+// this function needs to be determined at runtime.
 Meteor.methods({
   'bins.insert': function () {
     return Bins.insert({
-
+      createdAt: new Date(),
+      content: '',
+      sharedWith: [],
+      ownerId: this.userId,
     });
   },
+
+  'bins.remove': function (bin) {
+    return Bins.remove(bin);
+  },
 });
+
 export const Bins = new Mongo.Collection('bins');
